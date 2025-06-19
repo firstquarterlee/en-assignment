09e14a7f3ca6f07943cd618e52a70f06cef40a54
이상현 <firstquarterlee@gmail.com>
2025-06-20 02:06:19 +0900

    chore: 반응형 전역으로 관리

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'mobile': '375px',
+      'tablet': '810px',
+      'desktop': '1920px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
